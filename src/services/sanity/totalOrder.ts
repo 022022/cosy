ed11e402db23c8ae861643b82ac601c464153104
Zhenya <evgeegai@gmail.com>
2023-05-29 20:26:22 +0300

    serverless function for token security

--- src/services/sanity/totalOrder.ts
+++ src/services/sanity/totalOrder.ts
@@ -3,29 +3,26 @@ import { PROJECT_ID } from './publicData';
 import { DATASET } from './publicData';
 
 export async function sendOrder(totalOrder: TotalOrder) {
-    const tokenWithWriteAccess = process.env.REACT_APP_TOKEN;
-
-    const date = new Date();
-    const orderDate = date.toISOString();
+	const date = new Date();
+	const orderDate = date.toISOString();
 
 	const mutations = [
 		{
 			create: {
 				_type: 'orders',
-                orderDate,
-                ...totalOrder
+				orderDate,
+				...totalOrder,
 			},
 		},
 	];
+
+    const url = `https://${PROJECT_ID}.api.sanity.io/v2021-06-07/data/mutate/${DATASET}`;
+
 	await fetch(
-		`https://${PROJECT_ID}.api.sanity.io/v2021-06-07/data/mutate/${DATASET}`,
+		`/.netlify/functions/secure-fetch?url=${url}`,
 		{
 			method: 'post',
-			headers: {
-				'Content-type': 'application/json',
-				Authorization: `Bearer ${tokenWithWriteAccess}`,
-			},
 			body: JSON.stringify({ mutations }),
 		}
-	)
+	);
 }
